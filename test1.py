7426d11ecc06546557b93de4b11724b791f322df
dazhanova dasha <keepmolly66@gmail.com>
2023-11-15 19:14:39 +0300

    мой первый коммит

---
+++ test1.py
@@ -0,0 +1,11 @@
+import requests
+import  cmath
+
+q1 = input('Столица России?')
+print(q1)
+q2 = input('Столица Чебоксар?')
+print(q2)
+q3 = input('Столица Англии?')
+print(q3)
+q3 = input('Столица Бельгии?')
+print(q3)
